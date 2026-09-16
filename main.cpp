5dd45c79958794284f649aaffa7700084d79e468
Masorv <125551664+Masorv@users.noreply.github.com>
2026-09-16 12:30:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,31 +6,35 @@
  ************************/
 #include <iostream>
 #include <math.h>
+#include <windows.h>
 
 using namespace std;
 
 int main(){
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);
+
   const double pi = 3.14, g = 9.8;
   double M, m, k, radianAlpha, A, a1, a2;
   int alpha;
 
   cout << "Введите массу клина: ";
   cin >> M;
-  cout << "\nВведите массу бруска: ";
+  cout << "Введите массу бруска: ";
   cin >> m;
-  cout << "\nВведите коеффициент трения: ";
+  cout << "Введите коеффициент трения: ";
   cin >> k;
-  cout << "\nВведите угол: ";
+  cout << "Введите угол: ";
   cin >> alpha;
 
   radianAlpha = 40 * (pi / 180);
   A = M + m * (pow(sin(radianAlpha), 2) - k * sin(radianAlpha * 2) / 2);
 
   a1 = (g * M / A) * cos(radianAlpha) * (sin(radianAlpha) - k * cos(radianAlpha));
-  cout << '\n' << a1;
+  cout << a1 << '\n';
 
   a2 = (g * M / A) * sin(radianAlpha) * (cos(radianAlpha) + k * sin(radianAlpha));
-  cout << '\n' << a2;
+  cout << a2 << '\n';
 
   system ("pause");
   return 0;
