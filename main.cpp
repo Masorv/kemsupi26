0798f998b67c2304f23825c69fd457f7186c2650
Masorv <masorvun@gmail.com>
2026-09-21 21:08:05 +0700

    Small fix
    Changes:
     - True constants defined outside of main.
     - Comments added to a1 and a2.
     - cout statements merged into one.

--- main.cpp
+++ main.cpp
@@ -3,14 +3,16 @@
  * Название: Линейный алгоритм *
  * Вариант: 19                 *
  ******************************/
+
 #include <iostream>
 #include <math.h>
 
 using namespace std;
 
+const double pi = 3.14, g = 9.8;
+
 int main() {
  
-  const double pi = 3.14, g = 9.8;
   double wedgeMass, blockMass, frictionCoefficient, radianAlpha, A, a1, a2, alpha;
 
   cout << "Input mass of the wedge: ";
@@ -28,12 +30,12 @@ int main() {
 
   radianAlpha = alpha * (pi / 180.0);
   A = wedgeMass + blockMass * (pow(sin(radianAlpha), 2.0) - frictionCoefficient * sin(radianAlpha * 2.0) / 2.0);
-
+  //The friction is absent between the wedge and the horizontal plate
   a1 = (g * wedgeMass / A) * cos(radianAlpha) * (sin(radianAlpha) - frictionCoefficient * cos(radianAlpha));
+  //The friction is absent between the block and the wedge
   a2 = (g * wedgeMass / A) * sin(radianAlpha) * (cos(radianAlpha) + frictionCoefficient * sin(radianAlpha));
 
-  cout << a1 << '\n';
-  cout << a2 << '\n';
+  cout << a1 << '\n' << a2 << '\n';
 
   system("pause");
   return 0;
